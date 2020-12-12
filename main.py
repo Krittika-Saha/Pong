747b8d57499ea6c98874dda4783902ecc2541088
Krittika-Saha <krittika.saha.dev@gmail.com>
2020-12-12 17:51:58 +0530

    Challenge 6 completed 🤩

--- main.py
+++ main.py
@@ -24,8 +24,12 @@ while True:
   screen.update()
 
   #Detect collision with wall
-  if ball.ycor() > 300 or ball.ycor() < -300:
-    ball.bounce()
+  if ball.ycor() > 280 or ball.ycor() < -280:
+    ball.bounce_y()
+
+  #Detect collisions with paddle
+  if ball.distance(paddle_1) < 50 and ball.xcor() > 320 or ball.distance(paddle_2) < 50 and ball.xcor() < -320:
+      ball.bounce_x()
 
 
 
@@ -35,3 +39,40 @@ while True:
 
 
 screen.exitonclick()
+
+
+# from turtle import Screen, Turtle
+# from paddle import Paddle
+# from ball import Ball
+# from scoreboard import Scoreboard
+# import time
+
+# screen = Screen()
+# screen.bgcolor("black")
+# screen.setup(width=800, height=600)
+# screen.title("Pong")
+# screen.tracer(0)
+
+# r_paddle = Paddle((350, 0))
+# l_paddle = Paddle((-350, 0))
+# ball = Ball()
+# scoreboard = Scoreboard()
+
+# screen.listen()
+# screen.onkey(r_paddle.go_up, "Up")
+# screen.onkey(r_paddle.go_down, "Down")
+# screen.onkey(l_paddle.go_up, "w")
+# screen.onkey(l_paddle.go_down, "s")
+
+# game_is_on = True
+# while game_is_on:
+#     screen.update()
+#     ball.move()
+
+#     #Detect collision with wall
+#     if ball.ycor() > 280 or ball.ycor() < -280:
+#         ball.bounce_y()
+
+#     #Detect collision with paddle
+#     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+#         ball.bounce_x()
